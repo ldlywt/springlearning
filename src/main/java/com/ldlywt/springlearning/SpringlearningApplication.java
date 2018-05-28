495e4f3511282ebd209e15f854d1fe7ed0148c6c
wutao <ldlywt@163.com>
2018-05-28 20:15:04 +0800

    修改一些配置的错误

--- src/main/java/com/ldlywt/springlearning/SpringlearningApplication.java
+++ src/main/java/com/ldlywt/springlearning/SpringlearningApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ldlywt.springlearning.entity")
+@MapperScan("com.ldlywt.springlearning.mapper")
 public class SpringlearningApplication {
 
 	public static void main(String[] args) {
